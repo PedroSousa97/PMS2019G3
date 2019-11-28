e405071ad4bf059fe265155c6f1e8c5a234ac5ef
NinjaDragon98 <duartefigueiroa_98@hotmail.com>
2019-11-28 15:37:08 +0000

    Update Problema.java

--- Public/Problema.java
+++ Public/Problema.java
@@ -1,1 +1,25 @@
-public class 
+public class Problema{
+    private String sintomas;
+    private String causas;
+
+    public Problema(String sintomas, String causas){
+        this.sintomas=sintomas;
+        this.causas=causas;
+    }
+
+    public String getSintomas(){
+        return sintomas;
+    }
+
+    public void setSintomas(String sintomas){
+        this.sintomas=sintomas;
+    }
+
+    public String getCausas(String causas){
+        return causas;
+    }
+
+    public void setCausas(String causas){
+        this.causas=causas;
+    }
+}
